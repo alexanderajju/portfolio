b88d948125938fb35061c8bbcac32b9eb0ce68d6
alexanderajju <alexanderajju@gmail.com>
2020-08-08 13:31:38 +0530

    validation edited

--- validation.js
+++ validation.js
@@ -1,31 +1,51 @@
-$(document).ready(function () {
-  $("#submit-form").validate({
-    rules: {
-      name: {
-        required: true,
-        minlength: 5,
+$(document).ready(
+  (onsubmit = () => {
+    $("#submit-form").validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 5,
+        },
+
+        email: {
+          required: true,
+          email: true,
+        },
+        option: {
+          required: true,
+        },
+        subject: {
+          required: true,
+        },
+        mobile: {
+          required: true,
+          number: true,
+          minlength: 10,
+        },
       },
 
-      email: {
-        required: true,
-        email: true,
+      messages: {
+        name: {
+          required: "enter your name",
+        },
       },
-      option: {
-        required: true,
+
+      submitHandler: function (form) {
+        $.ajax({
+          url:
+            "https://script.google.com/macros/s/AKfycbxWtuIGHnUMC-ciRKbAvJJbRSXCgkyoaM-RfQoycA/exec",
+          data: $("#submit-form").serialize(),
+          method: "post",
+          success: function (response) {
+            alert("Form submitted successfully");
+            window.location.reload();
+            window.location.href = "index.html";
+          },
+          error: function (err) {
+            alert("Something Error");
+          },
+        });
       },
-      subject: {
-        required: true,
-      },
-      mobile: {
-        required: true,
-        number: true,
-        minlength: 10,
-      },
-    },
-    messages: {
-      name: {
-        required: "enter your name",
-      },
-    },
-  });
-});
+    });
+  })
+);
